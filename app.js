38dd83d3b455bdcfb02c6a6e8199f9964ca4dec2
jssarrazolaa <jssarrazolaa@unal.edu.co>
2020-10-27 15:26:50 -0500

    add style and form reset

--- app.js
+++ app.js
@@ -1,81 +1,83 @@
-document.getElementById('book').addEventListener('submit',addBook);
+const booksContainer = document.querySelector('#books-container');
 
+const myLibrary = [];
 
-const books_container = document.querySelector('#books-container');
-
-function addBook(e){
-    e.preventDefault();
-    let title = document.getElementById('title').value;
-    let author = document.getElementById('author').value;
-    let pages = document.getElementById('pages').value;
-    let status = document.getElementById('read').checked;
-    
-    console.log(status)
-    let book = new Book(title,author,pages,status);
-    
-    create(book,books_container);
-
-    addBookToLibrary(book);
+function Book(title, author, pages, status) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
 }
 
+Book.prototype.changeStatus = function changeStatus() {
+  if (this.status === true) {
+    this.status = false;
+  } else {
+    this.status = true;
+  }
+};
 
+function create(book) {
+  const cardContainer = document.createElement('div');
+  cardContainer.classList.add('card', 'w-25');
+  const cont = document.createElement('div');
+  cont.classList.add('card-body');
+  const title = document.createElement('h2');
+  title.classList.add('card-title');
+  const author = document.createElement('p');
+  author.classList.add('card-subtitle');
+  const pages = document.createElement('p');
+  pages.classList.add('card-text');
+  const read = document.createElement('p');
+  const readBtn = document.createElement('button');
+  readBtn.classList.add('btn', 'btn-primary');
+  if (book.status) {
+    readBtn.textContent = 'Not Read';
+  } else {
+    readBtn.textContent = 'Read';
+  }
 
+  title.textContent = book.title;
+  author.textContent = book.author;
+  pages.textContent = book.pages;
 
-let myLibrary = [];
+  booksContainer.appendChild(cont);
+  booksContainer.appendChild(cardContainer);
+  cardContainer.appendChild(cont);
+  cont.appendChild(title);
+  cont.appendChild(author);
+  cont.appendChild(pages);
+  cont.appendChild(read);
+  cont.appendChild(readBtn);
 
-function Book(title,author,pages,status) {
-    this.title=title,
-    this.author=author,
-    this.pages=pages,
-    this.status=status,
-    this.changeStatus=function(){
-        if(this.status===true){
-            this.status=false
-            
-        }else{
-            this.status=true
-            
-        }
+  readBtn.addEventListener('click', () => {
+    book.changeStatus();
+    if (book.status === true) {
+      readBtn.textContent = 'Not Read';
+    } else if (book.status === false) {
+      readBtn.textContent = 'Read';
     }
-}
-
-function create(book){
-    let cont = document.createElement('div');
-    let title = document.createElement('h2');
-    let author = document.createElement('p');
-    let pages = document.createElement('p');
-    let read = document.createElement('p');
-    let read_btn = document.createElement('button');
-    read_btn.textContent='Not Read'
-    
-
-    title.textContent = book.title;
-    author.textContent = book.author;
-    pages.textContent = book.pages;
-    
-
-    books_container.appendChild(cont);
-    cont.appendChild(title);
-    cont.appendChild(author);
-    cont.appendChild(pages);
-    cont.appendChild(read);
-    cont.appendChild(read_btn)
-    read_btn.addEventListener('click',function(e){
-       book.changeStatus()
-       console.log(book.status)
-       if(book.status===true){
-            read_btn.textContent='Not Read'
-           
-       }else if(book.status===false) {
-            read_btn.textContent='Read'
-       }
-        
-    
-    })
-   
+  });
 }
 
 function addBookToLibrary(book) {
-    myLibrary.push(book);
-    console.log(myLibrary);
+  myLibrary.push(book);
 }
+
+function addBook(e) {
+  e.preventDefault();
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
+  const status = document.getElementById('read').checked;
+
+  const book = new Book(title, author, pages, status);
+
+  create(book, booksContainer);
+
+  addBookToLibrary(book);
+
+  document.getElementById('book').reset();
+}
+
+document.getElementById('book').addEventListener('submit', addBook);
